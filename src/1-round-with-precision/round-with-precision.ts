6e8a0abf89bab25069e3365b43d684e685fa3a4d
gjoao98 <gjoao98@gmail.com>
2021-05-18 17:41:56 -0300

    Inicial Commit

---
+++ src/1-round-with-precision/round-with-precision.ts
@@ -0,0 +1,12 @@
+export const roundWithPrecision = (
+  value: number,
+  decimalPrecision: number
+): number => {
+  // Delete the next line, implementation goes there
+  const precision = decimalPrecision
+  const decimal = value;
+  const rounded = parseFloat(decimal.toFixed(precision))
+
+  console.log(rounded);
+  return rounded;
+};
